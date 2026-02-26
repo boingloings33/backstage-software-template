df39f28e86ac35acf8027bbd907a396544e3e168
Brenden Baio <baiolemi@gmail.com>
2026-02-26 12:04:04 -0500

    installed and templatize python app v1

---
+++ python-app/template/src/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import datetime
+import socket
+
+app = Flask (__name__)
+@app.route('/')
+@app.route('/api/v1/info')
+
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'message': "Keep it up! Backstage Integration test",
+        'deployed_on': 'Kubernetes'
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+        })
+
+@app.route('/api/v1/healthz')
+
+def health():
+    return jsonify({
+        'status': 'up'
+    }), 200
+
+if __name__ == '__main__':
+
+    app.run(host="0.0.0.0")
