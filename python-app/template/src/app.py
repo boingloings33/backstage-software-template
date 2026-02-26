db3f454723229c26f0e34141155b720a530947ed
Brenden Baio <baiolemi@gmail.com>
2026-02-26 15:32:40 -0500

    missing comma breaking app

--- python-app/template/src/app.py
+++ python-app/template/src/app.py
@@ -2,27 +2,25 @@ from flask import Flask, jsonify
 import datetime
 import socket
 
-app = Flask (__name__)
+app = Flask(__name__)
+
 @app.route('/')
 @app.route('/api/v1/info')
-
 def info():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M%p on %B %d, %Y"),
         'hostname': socket.gethostname(),
         'message': "Prod version 1.0",
-        'deployed_on': 'Kubernetes'
+        'deployed_on': 'Kubernetes',
         'env': '${{values.app_env}}',
-        'app_name': '${{values.app_name}}'
-        })
+        'app_name': '${{values.app_name}}',
+    })
 
 @app.route('/api/v1/healthz')
-
 def health():
     return jsonify({
         'status': 'up'
     }), 200
 
 if __name__ == '__main__':
-
     app.run(host="0.0.0.0")
